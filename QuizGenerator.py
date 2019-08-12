30506b65fc585729286b12082789d77dc6952df7
Michael Ma <mkma93@gmail.com>
2019-08-12 14:26:49 -0700

    Final Commit

--- QuizGenerator.py
+++ QuizGenerator.py
@@ -36,25 +36,25 @@ for quizNum in range(35):
 
 
 
-# Loop through all 50 states, making a question for each.
-for questionNum in range(50):
+    # Loop through all 50 states, making a question for each.
+    for questionNum in range(50):
 
-    # Get right and wrong answers.
-    correctAnswer = capitals[states[questionNum]]
-    wrongAnswers = list(capitals.values())
-    del wrongAnswers[wrongAnswers.index(correctAnswer)]
-    wrongAnswers = random.sample(wrongAnswers, 3)
-    answerOptions = wrongAnswers + [correctAnswer]
-    random.shuffle(answerOptions);
+        # Get right and wrong answers.
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions);
 
-    # Write the question and the answer options to the quiz file.
-    quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
-    for i in range(4):
-        quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
-    quizFile.write('\n')
+        # Write the question and the answer options to the quiz file.
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+        for i in range(4):
+            quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+        quizFile.write('\n')
 
-    # Write the answer key to a file.
-    answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+        # Write the answer key to a file.
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
 
-quizFile.close()
-answerKeyFile.close()
+    quizFile.close()
+    answerKeyFile.close()
